4d3ea98528862e8d3fda215fddefd53551cc689e
Bill Eager <beager@gmail.com>
2015-08-31 00:21:01 -0400

    tons of stuff, calling it

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -20,77 +20,99 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var errorView: UIView!
 
+    // Top Box Office URL
     let boxOfficeUrl = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
     
+    // Top DVD Rentals URL
     let topDvdUrl = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/e41513a57049e21bc6cf/raw/b490e79be2d21818f28614ec933d5d8f467f0a66/gistfile1.json")!
     
+    // The current data source URL
     var currentDataSourceUrl: NSURL?
     
+    // The last selected TabBar index, to make sure we don't load when you tap what's already selected
     var lastSelectedTabBarIndex: Int?
     
+    // The full list of movies
     var movies: [NSDictionary]?
     
+    // The filtered list of movies, for search filtering
     var filteredMovies: [NSDictionary]?
     
+    // UIRefreshControl
     var refreshControl: UIRefreshControl!
     
-    var tapGesture: UITapGestureRecognizer!
+    // A UITapGestureRecognizer to assist with search dismissal
+    var tapTableViewGesture: UITapGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Initialize movies and filteredMovies
+        movies = []
+        filteredMovies = []
+        
+        // Default data source to boxOfficeUrl and tab bar index
+        // TODO: programmatically set this by examining UITabBar state?
         currentDataSourceUrl = boxOfficeUrl
         lastSelectedTabBarIndex = 0
+        tabBar.selectedItem = topBoxOfficeTabBarItem
+        
+        // Set a whole lot of sources and delegates to self
         tableView.dataSource = self
         tableView.delegate = self
         searchBar.delegate = self
         tabBar.delegate = self
         
+        // Add refresh control to the tableView
         refreshControl = UIRefreshControl()
         refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh...", attributes: [NSForegroundColorAttributeName: UIColor.orangeColor()])
         refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
         tableView.insertSubview(refreshControl, atIndex:0)
-
+        
+        // Add tap gesture recognizer to dismiss search when interacting with the results
+        tapTableViewGesture = UITapGestureRecognizer(target: self, action: Selector("hideKeyboard"))
+        tapTableViewGesture.cancelsTouchesInView = false
+        tableView.addGestureRecognizer(tapTableViewGesture)
+        
+        // Set loader state and load data
         SwiftLoader.show(title: "Loading...", animated: true)
         self.loadData()
-        
-        tabBar.selectedItem = topBoxOfficeTabBarItem
-        
-        tapGesture = UITapGestureRecognizer(target: self, action: Selector("hideKeyboard"))
-        tapGesture.cancelsTouchesInView = false
-        tableView.addGestureRecognizer(tapGesture)
     }
     
+    // action triggered by UITapGestureRecognizer
     func hideKeyboard() {
         tableView.endEditing(true)
     }
     
+    // Loads data
     func loadData() {
-        let request = NSURLRequest(URL: currentDataSourceUrl!)
+        let request = NSURLRequest(URL: currentDataSourceUrl!, cachePolicy: NSURLRequestCachePolicy.ReturnCacheDataElseLoad, timeoutInterval: 30.0)
 
+        // Hide the error view if it was shown
         errorView.hidden = true
         
+        // Make the call
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
             
             if ((error) != nil) {
+                // Error loading, hide the loader and show the error view
+                SwiftLoader.hide()
                 self.errorView.hidden = false
-                return
             } else {
-                self.delay(0.5, closure: {
-                    let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
-                    if let json = json {
-                        self.movies = json["movies"] as? [NSDictionary]
-                        self.filteredMovies = self.movies
-                        self.reloadTableViewIfDataExists()
-                    }
-                    
-                    SwiftLoader.hide()
-                    self.refreshControl.endRefreshing()
-                })
+                // Everything was good, unpack the JSON
+                let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
+                if let json = json {
+                    self.movies = json["movies"] as? [NSDictionary]
+                    self.filteredMovies = self.movies
+                }
             }
+            // End refreshing and reload table view
+            self.refreshControl.endRefreshing()
+            self.reloadTableViewIfDataExists()
         }
     }
     
+    // Delay function I got from somewhere, used to delay setting vars
     func delay(delay: Double, closure:()->()) {
         dispatch_after(
             dispatch_time(
@@ -99,6 +121,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             ), dispatch_get_main_queue(), closure)
     }
     
+    // When refresh is triggered, change the title and load the data
     func onRefresh() {
         refreshControl.attributedTitle = NSAttributedString(string: "Loading data...", attributes: [NSForegroundColorAttributeName: UIColor.orangeColor()])
         self.loadData()
@@ -123,8 +146,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movieDetailsViewController = segue.destinationViewController as! MovieDetailsViewController
         movieDetailsViewController.movie = movie
     }
-    
-    
+
+    // numberOfRowsInSection
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let filteredMovies = filteredMovies {
             return filteredMovies.count
@@ -139,16 +162,20 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
+        // Instantiate model
         let movie = Movie(movie: filteredMovies![indexPath.row])
         
         cell.frame = CGRectMake(0, 0, 300.0, 300.0)
         
+        // Set labels and UIImageView
         cell.titleLabel.text = movie.title
         cell.synopsisLabel.text = movie.synopsis
         cell.posterView.image = UIImage()
         
+        // Load thumbnail image here
         let url = NSURL(string: movie.poster)
         
+        // Success callback
         let imageRequestSuccess = {
             (request : NSURLRequest!, response : NSHTTPURLResponse!, image : UIImage!) -> Void in
             cell.posterView.image = image;
@@ -157,42 +184,66 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 cell.posterView.alpha = 1.0
             })
         }
+        
+        // Failure callback
         let imageRequestFailure = {
             (request : NSURLRequest!, response : NSHTTPURLResponse!, error : NSError!) -> Void in
             NSLog("imageRequestFailure")
         }
 
-        cell.posterView.setImageWithURLRequest(NSURLRequest(URL: url!), placeholderImage: nil, success: imageRequestSuccess, failure: imageRequestFailure)
+        // Execute the call
+        cell.posterView.setImageWithURLRequest(NSURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.ReturnCacheDataElseLoad, timeoutInterval: 30.0), placeholderImage: nil, success: imageRequestSuccess, failure: imageRequestFailure)
+        
+        // If we've loaded a viewport worth of data, dismiss the loader
+        if (indexPath.row == tableView.indexPathsForVisibleRows()?.last?.row) {
+            SwiftLoader.hide()
+        }
+        
+        // Custom select state for cell
+        var bgColorView = UIView()
+        bgColorView.backgroundColor = UIColor.orangeColor()
+        cell.selectedBackgroundView = bgColorView
         
         return cell
     }
 
+    // Deselect active row on tap
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
+    // When tabBar item selection happens
     func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem!) {
+        // If no change, return
         if (item.tag == lastSelectedTabBarIndex) {
             return
         }
+        
+        // Set the proper data source
         if (item.tag == 0) {
             currentDataSourceUrl = boxOfficeUrl
         } else if (item.tag == 1) {
             currentDataSourceUrl = topDvdUrl
         }
+
+        // Do some reloading stuff
         filteredMovies = []
         reloadTableViewIfDataExists()
         searchBar.text = ""
         SwiftLoader.show(title: "Loading...", animated: true)
         loadData()
+        
+        // Set title of this view to be the title of whichever item you selected
         self.title = item.title
         lastSelectedTabBarIndex = item.tag
     }
     
+    // Callback on text change in Search
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
         filterContentForSearchText(searchText)
     }
     
+    // Reloads the table view if data exists, or shows the no results view
     func reloadTableViewIfDataExists() {
         if (self.filteredMovies?.isEmpty == true) {
             noResultsView.hidden = false
@@ -202,6 +253,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         }
     }
     
+    // Filters search content
     func filterContentForSearchText(searchText: String) {
         // Filter the array using the filter method
         if (searchText == "") {
@@ -217,13 +269,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     // MARK: UISearchBarDelegate methods
     
+    // Callback on end editing for search
     func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        // Have to delay here or the touch will be picked up by the tableView and respond
+        // TODO: Not do this?
         delay(0.1, closure: {
-            self.tapGesture.cancelsTouchesInView = false
+            self.tapTableViewGesture.cancelsTouchesInView = false
         })
     }
     
+    // Callback on start editing for search
     func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
-        tapGesture.cancelsTouchesInView = true
+        tapTableViewGesture.cancelsTouchesInView = true
     }
 }
--- Pannd/AppDelegate.swift
+++ Pannd/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        let URLCache = NSURLCache(memoryCapacity: 4 * 1024 * 1024, diskCapacity: 2 * 1024 * 1024, diskPath: nil)
+        NSURLCache.setSharedURLCache(URLCache)
         return true
     }
 
